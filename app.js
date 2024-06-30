dde0dd4c24bffa6b38cf98cc1046bde93bcbf684
Sadiq <sediqabdullahi04@gmail.com>
2024-06-30 23:05:55 +0100

    d

--- app.js
+++ app.js
@@ -8,16 +8,15 @@ app.get("/api/hello", (req, res) => {
     const { visitor_name } = req.query
     if (!visitor_name) res.status(401).json({ message: "Visitor's name is required" })
     else {
-        const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-        ipinfo((ip, (err, loc) => {
+        ipinfo((req.ip, (err, loc) => {
             if (err) {
                 res.status(404).json({ message: "Cannot get location" })
             }
             else {
                 res.json({
-                    client_ip: ip,
+                    client_ip: req.ip,
                     location: loc.city,
-                    greeting: `Hello, ${visitor_name}!, the temperature is 11 degrees Celcius in New York`
+                    greeting: `Hello, ${visitor_name}!, the temperature is 11 degrees Celcius in ${loc.city}`
                 })
             }
         }))
