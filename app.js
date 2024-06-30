e554736c164ad3ffaa300f4e602c891614e35113
Sadiq <sediqabdullahi04@gmail.com>
2024-06-30 23:21:19 +0100

    geoip2

--- app.js
+++ app.js
@@ -1,25 +1,21 @@
 const express = require("express")
 const dotenv = require("dotenv")
+const geoip = require("geoip-lite2")
 dotenv.config()
-const ipinfo = require("ipinfo")
 const app = express()
 
 app.get("/api/hello", (req, res) => {
     const { visitor_name } = req.query
     if (!visitor_name) res.status(401).json({ message: "Visitor's name is required" })
     else {
-        ipinfo((req.ip, (err, loc) => {
-            if (err) {
-                res.status(404).json({ message: "Cannot get location" })
-            }
-            else {
-                res.json({
-                    client_ip: req.ip,
-                    location: loc.city,
-                    greeting: `Hello, ${visitor_name}!, the temperature is 11 degrees Celcius in ${loc.city}`
-                })
-            }
-        }))
+        const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+        const geo = geoip.lookup(ip)
+        res.json({
+            client_ip: ip,
+            location: geo.city,
+            greeting: `Hello, ${visitor_name}!, the temperature is 11 degrees Celcius in ${geo.city}`
+        })
+
     }
 })
 
